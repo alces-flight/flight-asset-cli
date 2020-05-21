de43a276a410d29e82113894a0e7ae286909fb37
William McCumsite <william.mccumstie@gmail.com>
2020-05-21 17:36:08 +0100

    Align the headers from the show "table"

--- lib/flight_asset/commands/concerns/has_table_element.rb
+++ lib/flight_asset/commands/concerns/has_table_element.rb
@@ -33,11 +33,22 @@ module FlightAsset
 
         included do
           after(if: :tty?) do
+            # Local caches the element so it can be reused
             element = table_element
-            table_procs.each do |key, proc|
+
+            # Converts procs to prettified data
+            data = table_procs.map do |key, proc|
               header = Paint[key + ':', '#2794d8']
               value = Paint[proc.call(element), :green]
-              puts "#{header} #{value}"
+              [header, value]
+            end
+
+            # Determines the maximum width header for padding
+            max = data.max { |h, v| h.length }[0].length
+
+            # Prints the data with required padding
+            data.each do |header, value|
+              puts "#{' ' * (max - header.length)}#{header} #{value}"
             end
           end
 
